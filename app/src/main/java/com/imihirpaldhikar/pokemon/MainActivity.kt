d01a26adad372859179c2d6e3e44d61f0ee168ed
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-12-03 11:05:49 +0530

    Added the required Assets, fonts.

--- app/src/main/java/com/imihirpaldhikar/pokemon/MainActivity.kt
+++ app/src/main/java/com/imihirpaldhikar/pokemon/MainActivity.kt
@@ -3,10 +3,16 @@ package com.imihirpaldhikar.pokemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.imihirpaldhikar.pokemon.ui.theme.PokemonTheme
 
@@ -16,7 +22,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokemonTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                ) {
                     Greeting("Android")
                 }
             }
@@ -26,7 +37,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center, content = {
+        Text(text = "Hello $name!")
+    })
 }
 
 @Preview(showBackground = true)
